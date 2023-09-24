5d47f5c391f5b9a04fc42d68704a72c3f746100f
yeondori <93027942+yeondori@users.noreply.github.com>
2023-09-24 16:23:12 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Id = sc.nextLine();
+        
+        if (Id.equals(Id)) {
+        		System.out.println(Id+"??!");}    
+        }  	
+    }
