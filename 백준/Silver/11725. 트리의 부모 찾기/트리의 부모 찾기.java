7777ebc5cd025171a06532ab4a36b54f8b4384b3
yeondori <93027942+yeondori@users.noreply.github.com>
2024-01-24 21:29:35 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2320 ms, Memory: 125732 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+
+    static int N;
+    static boolean[] visited;
+    static int[] sol;
+    static ArrayList<List<Integer>> tree;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        sol = new int[N+1];
+        visited = new boolean[N+1];
+
+        tree = new ArrayList<>();
+        for(int i=0; i<= N; i++) {
+            tree.add(new ArrayList<>());
+        }
+
+        for(int i=1; i<N; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+
+            tree.get(input[0]).add(input[1]);
+            tree.get(input[1]).add(input[0]);
+        }
+        dfs(1);
+        printSolution();
+    }
+
+    private static void dfs(int v) {
+        visited[v] = true;
+
+        for (int i : tree.get(v)){
+            if (!visited[i]){
+                sol[i] = v;
+                dfs(i);
+            }
+        }
+    }
+
+    private static void printSolution() {
+        Arrays.stream(sol)
+                .skip(2)
+                .forEach(System.out::println);
+    }
+}
