798ae887f716383628f8f62abc873a827651c7d4
yeondori <93027942+yeondori@users.noreply.github.com>
2024-02-08 02:08:14 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,41 +1,43 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int paperWidth = 100;
-    static final int paperLength = 100;
-    static final int coloredPaperWidth = 10;
-    static final int coloredPaperLength = 10;
+    static final int PAPER_WIDTH = 100;
+    static final int PAPER_LENGTH = 100;
+    static final int COLORED_PAPER_WIDTH = 10;
+    static final int COLORED_PAPER_LENGTH = 10;
+    static int[][] paper = new int[PAPER_WIDTH][PAPER_LENGTH];
 
     public static void main(String[] args) throws IOException {
-
-        int[][] paper = new int [paperWidth][paperLength];
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        for (int i=0; i<N; i++) {
-            StringTokenizer xy = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(xy.nextToken())-1;
-            int y = Integer.parseInt(xy.nextToken())-1;
-            paper = fill(paper, x, y);
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            fill(paper, x, y);
         }
 
+        calculate();
+    }
+
+    private static void calculate() {
         int area = 0;
-        for (int i=0; i<paperWidth; i++) {
-            for (int j=0; j<paperLength; j++) {
-                if (paper[i][j] == 1) area +=1;
+        for (int i = 0; i < PAPER_WIDTH; i++) {
+            for (int j = 0; j < PAPER_LENGTH; j++) {
+                if (paper[i][j] == 1) area += 1;
             }
         }
-
         System.out.println(area);
     }
 
-    public static int[][] fill(int[][] matrix, int x, int y) {
-        for (int i=0; i<coloredPaperWidth; i++) {
-            for (int j=0; j<coloredPaperLength; j++) {
-                matrix[x+i][y+j] = 1;
+    public static void fill(int[][] matrix, int x, int y) {
+        for (int i = 0; i < COLORED_PAPER_WIDTH; i++) {
+            for (int j = 0; j < COLORED_PAPER_LENGTH; j++) {
+                matrix[x + i][y + j] = 1;
             }
         }
-        return matrix;
     }
 }
