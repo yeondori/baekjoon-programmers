e4ed954f511ba88b771eda5cf77d3f16c9b5d835
yeondori <93027942+yeondori@users.noreply.github.com>
2024-02-12 21:55:06 +0900

    [Gold III] Title: 우주 탐사선, Time: 124 ms, Memory: 11860 KB -BaekjoonHub

---
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, K, answer;
+    static int[][] T;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        T = new int[N][N];
+        visited = new boolean[N];
+        answer = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int min = 1000;
+            for (int j = 0; j < N; j++) {
+                T[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    T[i][j] = Math.min(T[i][j], T[i][k] + T[k][j]);
+                }
+            }
+        }
+
+        visited[K] = true;
+        dfs(K, 0, 0);
+        System.out.println(answer);
+    }
+
+    static void dfs(int node, int cnt, int cost) {
+        if (cnt == N - 1) {
+            answer = Math.min(answer, cost);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i, cnt + 1, cost + T[node][i]);
+                visited[i] = false;
+            }
+        }
+    }
+}
