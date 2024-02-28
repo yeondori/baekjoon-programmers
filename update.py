8baae4ac7e1097a109990e72f391a0b117239a46
yeondori <93027942+yeondori@users.noreply.github.com>
2024-02-28 23:50:50 +0900

    Update update.py

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ from urllib import parse
 HEADER="""# 
 # SWEA & 백준 & 프로그래머스 문제 풀이 목록
 
-[README 자동화 코드 참고]https://velog.io/@betterfuture4/%EA%B9%83%ED%97%88%EB%B8%8C-%EC%9E%94%EB%94%94%EC%97%90-%EB%AC%BC-%EC%A3%BC%EA%B8%B0feat.BaekjoonHub%EB%A1%9C-%EB%B0%B1%EC%A4%80-%EB%AC%B8%EC%A0%9C-%EC%9E%90%EB%8F%99-%EC%97%85%EB%A1%9C%EB%93%9C)
+[README 자동화 코드 참고](https://velog.io/@betterfuture4/%EA%B9%83%ED%97%88%EB%B8%8C-%EC%9E%94%EB%94%94%EC%97%90-%EB%AC%BC-%EC%A3%BC%EA%B8%B0feat.BaekjoonHub%EB%A1%9C-%EB%B0%B1%EC%A4%80-%EB%AC%B8%EC%A0%9C-%EC%9E%90%EB%8F%99-%EC%97%85%EB%A1%9C%EB%93%9C)
 """
 
 def main():
