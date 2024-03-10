13db83de389fb6d0447722842196f615ef8ed05a
yeondori <93027942+yeondori@users.noreply.github.com>
2024-03-10 13:58:26 +0900

    [Gold IV] Title: 올라올라, Time: 444 ms, Memory: 164584 KB -BaekjoonHub

---
+++ 백준/Gold/31235. 올라올라/올라올라.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int prev = Integer.parseInt(st.nextToken());
+        int k = 1;
+        int answer = 1;
+        for (int i = 1; i < N; i++) {
+            int cur = Integer.parseInt(st.nextToken());
+            if (prev > cur) {
+                ++k;
+            } else {
+                prev = cur;
+                answer = Math.max(k, answer);
+                k = 1;
+            }
+        }
+        System.out.println(Math.max(k,answer));
+    }
+}
