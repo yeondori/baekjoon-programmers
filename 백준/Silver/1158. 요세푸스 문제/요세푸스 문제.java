e547c249b8d25211dee8061fcf1aca0b821d8778
yeondori <93027942+yeondori@users.noreply.github.com>
2024-03-17 01:49:34 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 228 ms, Memory: 13712 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,30 +1,39 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args)  throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder answer = new StringBuilder("<");
+	static int N, K;
+	static Deque<Integer> peoples;
+	static StringBuilder answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		answer = new StringBuilder("<");
+		peoples = new ArrayDeque<Integer>();
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		for (int i = 1; i <= N; i++) {
+			peoples.add(i);
+		}
+		
+		getAnswer();
+		System.out.println(answer);
+	}
 
-        ArrayList arr = new ArrayList();
-        int N = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int ptr = 0;
-
-        for (int i=1; i<=N; i++) arr.add(i);
-
-        while(!arr.isEmpty()) {
-            ptr += x-1;
-            ptr = ptr%arr.size();
-
-            answer.append(arr.get(ptr));
-            if (arr.size()>1) answer.append(", ");
-            arr.remove(ptr);
-        }
-        answer.append(">");
-        System.out.println(answer);
-    }
+	private static void getAnswer() {
+		while(peoples.size()>1) {
+			for (int i = 0; i < K-1; i++) {
+				peoples.offer(peoples.poll());
+			}
+			answer.append(peoples.poll()).append(", ");
+		}
+		answer.append(peoples.poll()).append(">");
+	}
 }
