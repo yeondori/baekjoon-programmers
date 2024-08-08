21c5b40cbdd1a41097bbcdfc445125cf3854219f
yeondori <93027942+yeondori@users.noreply.github.com>
2024-08-08 22:21:40 +0900

    [Gold III] Title: 아침 산책, Time: 1004 ms, Memory: 111424 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.java
+++ 백준/Gold/21606. 아침 산책/아침 산책.java
@@ -1,9 +1,11 @@
 import java.util.*;
 import java.io.*;
 
+//https://prodyou.tistory.com/16
 public class Main {
     static ArrayList<Integer>[] graph;
     static boolean[] isIndoor;
+    static boolean[] visited;
     static long result = 0;
 
     public static void main(String[] args) throws IOException {
@@ -12,6 +14,7 @@ public class Main {
         String A = br.readLine();
 
         isIndoor = new boolean[N + 1];
+        visited = new boolean[N + 1];
         for (int i = 0; i < N; i++) {
             isIndoor[i + 1] = A.charAt(i) == '1';
         }
@@ -27,27 +30,31 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
             graph[u].add(v);
             graph[v].add(u);
+            if (isIndoor[u] && isIndoor[v]) {
+                result += 2; // 실내끼리 인접했을 경우 경로를 2개 더해준다.
+            }
         }
 
         for (int i = 1; i <= N; i++) {
-            if (isIndoor[i]) {
-                dfs(i, i, 0);
+            if (!isIndoor[i] && !visited[i]) { // 실외를 기준으로 생각
+                int indoor = dfs(i);
+                result += (long) indoor * (indoor - 1);
             }
         }
 
         System.out.println(result);
     }
 
-    static void dfs(int start, int current, int parent) {
-        if (isIndoor[current] && current != start) {
-            result++;
-            return;
-        }
-
-        for (int next : graph[current]) {
-            if (next != parent) {
-                dfs(start, next, current);
+    static int dfs(int v) {
+        visited[v] = true;
+        int indoor = 0;
+        for (int next : graph[v]) {
+            if (isIndoor[next]) {
+                indoor++;
+            } else if (!visited[next]) {
+                indoor += dfs(next);
             }
         }
+        return indoor;
     }
 }
